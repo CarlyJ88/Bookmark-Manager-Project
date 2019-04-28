46071013c95a0d90d727ce63c29d5320bb599e4b
Carly <carly.jenkinson65@googlemail.com>
2019-04-28 19:58:58 +0100

    Added a title column to the database. Title now shown instead of url on bookmarks page. Title links to specified website.

--- spec/bookmarks_spec.rb
+++ spec/bookmarks_spec.rb
@@ -4,24 +4,31 @@ describe Bookmarks do
 
   describe '.all' do
     it 'returns a list of bookmarks' do
+      connection = PG.connect(dbname: 'bookmark_manager_test')
 
-      Bookmarks.create(url: "http://www.makersacademy.com")
-      Bookmarks.create(url: "http://www.destroyallsoftware.com")
-      Bookmarks.create(url: "http://www.google.com")
-
+      bookmark = Bookmarks.create(url: 'http://www.makersacademy.com', title: "Makers Academy")
+      Bookmarks.create(url: 'http://www.destroyallsoftware.com', title: "Destroy all Software")
+      Bookmarks.create(url: 'http://www.google.com', title: "Google")
+      
       bookmarks = Bookmarks.all
 
-      expect(bookmarks).to include("http://www.makersacademy.com")
-      expect(bookmarks).to include("http://www.destroyallsoftware.com")
-      expect(bookmarks).to include("http://www.google.com")
+      expect(bookmarks.length).to eq 3
+      expect(bookmarks.first).to be_a Bookmarks
+      expect(bookmarks.first.id).to eq bookmark.id
+      expect(bookmarks.first.title).to eq ("Makers Academy")
+      expect(bookmarks.first.url).to eq 'http://www.makersacademy.com'
     end
   end
 
   describe '.create' do
     it 'creates a new bookmark' do
-      Bookmarks.create(url: 'http://www.testbookmark.com')
+      bookmark = Bookmarks.create(url: 'http://www.testbookmark.com', title: 'Test bookmark')
+      persisted_data = PG.connect(dbname: 'bookmark_manager_test').query("SELECT * FROM bookmarks WHERE id = #{bookmark.id};")
 
-      expect(Bookmarks.all).to include 'http://www.testbookmark.com'
+      expect(bookmark).to be_a Bookmarks
+      expect(bookmark.id).to eq persisted_data.first['id']
+      expect(bookmark.title).to eq ("Test bookmark")
+      expect(bookmark.url).to eq 'http://www.testbookmark.com'
     end
   end
 end
--- spec/features/viewing_bookmarks_spec.rb
+++ spec/features/viewing_bookmarks_spec.rb
@@ -10,15 +10,15 @@ end
 feature 'viewing bookmarks' do
   scenario 'user can see a list of bookmarks' do
 
-    Bookmarks.create(url: "http://www.makersacademy.com")
-    Bookmarks.create(url: "http://www.destroyallsoftware.com")
-    Bookmarks.create(url: "http://www.google.com")
+    Bookmarks.create(url: "http://www.makersacademy.com", title: "Makers Academy")
+    Bookmarks.create(url: "http://www.destroyallsoftware.com", title: "Destroy all Software")
+    Bookmarks.create(url: "http://www.google.com", title: "Google")
 
     visit('/bookmarks')
 
-    expect(page).to have_content("http://www.makersacademy.com")
-    expect(page).to have_content("http://www.destroyallsoftware.com")
-    expect(page).to have_content("http://www.google.com")
+    expect(page).to have_link("Makers Academy", href: "http://www.makersacademy.com")
+    expect(page).to have_link("Destroy all Software", href: "http://www.destroyallsoftware.com")
+    expect(page).to have_link("Google", href: "http://www.google.com")
   end
 end
 
@@ -26,8 +26,9 @@ end
     scenario 'A user can add a new bookmark to Bookmark Manager' do
       visit('/bookmarks/new')
       fill_in('url', with: 'http://testbookmark.com')
+      fill_in('title', with: 'Test bookmark')
       click_button('Submit')
 
-      expect(page).to have_content 'http://testbookmark.com'
+      expect(page).to have_content 'Test bookmark'
     end
   end
